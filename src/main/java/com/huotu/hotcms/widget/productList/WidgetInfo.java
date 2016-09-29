74424b12c1791b2e1b489ca133c1cedc9e639ebb
lihuaixin <1072931117@qq.com>
2016-09-29 10:12:38 +0800

    init

---
+++ src/main/java/com/huotu/hotcms/widget/productList/WidgetInfo.java
@@ -0,0 +1,105 @@
+/*
+ * 版权所有:杭州火图科技有限公司
+ * 地址:浙江省杭州市滨江区西兴街道阡陌路智慧E谷B幢4楼
+ *
+ * (c) Copyright Hangzhou Hot Technology Co., Ltd.
+ * Floor 4,Block B,Wisdom E Valley,Qianmo Road,Binjiang District
+ * 2013-2016. All rights reserved.
+ */
+
+package com.huotu.hotcms.widget.productList;
+import com.huotu.hotcms.widget.ComponentProperties;
+import com.huotu.hotcms.widget.PreProcessWidget;
+import com.huotu.hotcms.widget.Widget;
+import com.huotu.hotcms.widget.WidgetStyle;
+import me.jiangcai.lib.resource.service.ResourceService;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+
+/**
+ * @author CJ
+ */
+public class WidgetInfo implements Widget,PreProcessWidget{
+
+
+    @Override
+    public String groupId() {
+        return "com.huotu.hotcms.widget.productList";
+    }
+
+    @Override
+    public String widgetId() {
+        return "productList";
+    }
+
+    @Override
+    public String name(Locale locale) {
+        if (locale.equals(Locale.CHINA)) {
+            return "产品列表";
+        }
+        return "productList";
+    }
+
+    @Override
+    public String description(Locale locale) {
+        if (locale.equals(Locale.CHINA)) {
+            return "这是一个产品列表组件，你可以对组件进行自定义修改。";
+        }
+        return "This is a productList,  you can make custom change the component.";
+    }
+
+    @Override
+    public String dependVersion() {
+        return "1.0-SNAPSHOT";
+    }
+
+    @Override
+    public WidgetStyle[] styles() {
+        return new WidgetStyle[]{new DefaultWidgetStyle()};
+    }
+
+    @Override
+    public Resource widgetDependencyContent(MediaType mediaType){
+        if (mediaType.equals(Widget.Javascript))
+            return new ClassPathResource("js/widgetInfo.js", getClass().getClassLoader());
+        return null;
+    }
+
+    @Override
+    public Map<String, Resource> publicResources() {
+        Map<String, Resource> map = new HashMap<>();
+        map.put("thumbnail/defaultStyleThumbnail.png",new ClassPathResource("thumbnail/defaultStyleThumbnail.png",getClass().getClassLoader()));
+        return map;
+    }
+
+    @Override
+    public void valid(String styleId, ComponentProperties componentProperties) throws IllegalArgumentException {
+        WidgetStyle style = WidgetStyle.styleByID(this,styleId);
+        //加入控件独有的属性验证
+
+    }
+
+    @Override
+    public Class springConfigClass() {
+        return null;
+    }
+
+
+    @Override
+    public ComponentProperties defaultProperties(ResourceService resourceService) throws IOException {
+        ComponentProperties properties = new ComponentProperties();
+        return properties;
+    }
+
+    @Override
+    public void prepareContext(WidgetStyle style, ComponentProperties properties, Map<String, Object> variables, Map<String, String> parameters) {
+
+    }
+}
